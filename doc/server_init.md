92f0106df78b2b49d0fb647ae3936f739ad6d51e
icomico.alexdu <alexdu@comicool.cn>
2016-08-17 19:17:14 +0800

    add server_init.md server_init.sh

---
+++ doc/server_init.md
@@ -0,0 +1,2 @@
+    yum install -y zsh
+    yum install -y git
---
+++ shell/server_init.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+git clone https://github.com/Alex-duzhichao/server.git
