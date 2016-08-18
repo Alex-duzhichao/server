d3cb686d51e83907faa6a74648197c51682d0404
icomico.alexdu <alexdu@comicool.cn>
2016-08-18 17:38:31 +0800

    .

--- shell/install_zsh.sh
+++ shell/install_zsh.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #install zsh
 	sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-	sh ~/server/shell/update_zshrc.sh
+	zsh ~/server/shell/update_zshrc.sh
