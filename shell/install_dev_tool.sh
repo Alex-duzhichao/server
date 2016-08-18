90d9a493956b9722fe04786e285ff99b9b014c54
icomico.alexdu <alexdu@comicool.cn>
2016-08-18 15:03:11 +0800

    .

---
+++ shell/install_dev_tool.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+yum install -y sudo
+yum install -y zsh
+#install dev tools
+yum groupinstall -y "Development Tools"
--- shell/install_vim.sh
+++ shell/install_vim.sh
@@ -1,21 +1,5 @@
-初始一台服务器 
-==============  
-
-# install git #
-   	yum install -y git
-
-# pull git source #	
-	git clone https://github.com/Alex-duzhichao/server.git;chmod +x ~/server/shell/*.sh;
-
-# install dev tool #
-	sh ~/server/shell/install_dev_tool.sh
-
-# install oh-my-zsh #
-	sh ~/server/shell/install_zsh.sh
-
-
-
-	#install vim
+#!/bin/bash
+#install vim
 	####### lua #######
 	yum install -y lua-devel ncurses-devel
 	wget http://luajit.org/download/LuaJIT-2.0.4.tar.gz
